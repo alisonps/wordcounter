391bede67c5c4509ecf0478edd15a855b4b45c43
Alison Polton-Simon <alison.polton.simon@gmail.com>
2017-01-23 19:36:30 -0800

    Display count instead of id on results page'

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from bs4 import BeautifulSoup
 from rq import Queue
 from rq.job import Job
 from worker import conn
+from flask import jsonify
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -36,9 +37,15 @@ def index():
 def get_results(job_key):
 	job = Job.fetch(job_key, connection=conn)
 	if job.is_finished:
-		return str(job.result), 200
+		result = Result.query.filter_by(id=job.result).first()
+		results = sorted(
+			result.result_no_stop_words.items(),
+			key=operator.itemgetter(1),
+			reverse=True
+		)
+		return jsonify(results), 200
 	else:
-		return "Nay!", 200
+		return "Nay!", 202
 
 
 def parse_words_from_text(raw_text):
